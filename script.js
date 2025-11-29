cd1b17e4d711a8c5ec099435878118a6cd5e837f
Grace Adaeze Okengwu <136967329+docgany@users.noreply.github.com>
2025-11-29 08:47:14 +0000

    Objective is to build a clock using javascript date object and in-built methods to get the hours,minutes and seconds. And finally, using setInterval() to update it every second.

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Project 5: Build a digital clock using JavaScript.
+
+    Requirements:
+-Create a Github repository for this project. 
+- Display the hours, minutes, and seconds.
+- The clock should run by itself. (Hint: look at JavaScript setInterval function)
+    - Google "javascript date get time" to see the built -in JavaScript helpers you can use.
+- The clock should automatically update the time every second.*/
+
