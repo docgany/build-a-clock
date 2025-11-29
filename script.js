edc495348e6a161e38eabfba52833725fefc4cd2
Grace Adaeze Okengwu <136967329+docgany@users.noreply.github.com>
2025-11-29 10:54:49 +0000

    Final commit. Still practicing getting the toggle functionality right without manual formatting, but rather using .toLocaleTimeString() with options property. Yet to perfect it. This journey is not about getting it right, but rather understanding the fundamentals and logic.

--- script.js
+++ script.js
@@ -7,3 +7,16 @@
     - Google "javascript date get time" to see the built -in JavaScript helpers you can use.
 - The clock should automatically update the time every second.*/
 
+let is24Hour = true;
+
+function updateClock() {
+    const now = new Date();
+    let clockEl = document.getElementById("clock");
+    const timeStr = now.toLocaleTimeString();
+    clockEl.textContent = timeStr;
+    let timeInterval = setInterval(updateClock, 1000);
+
+    clearInterval(timeInterval);
+}
+
+updateClock();
